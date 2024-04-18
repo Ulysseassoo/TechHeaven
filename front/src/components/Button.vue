d50d232175a34961b23964da26032a70c96b1f20
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-04-18 23:45:15 +0200

    Added vuetify

---
+++ front/src/components/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ content: string }>();
+
+</script>
+
+<template>
+  <v-btn>{{ content }}</v-btn>
+</template>
---
+++ front/src/plugins/vuetify.ts
@@ -0,0 +1,17 @@
+// import { ThemeDefinition, createVuetify } from "vuetify";
+
+// const customTheme: ThemeDefinition = {
+//     dark: false,
+//     colors: {
+//         background: '#FFFFFF',
+//     },
+// }
+
+// export const theme  = ({
+//     theme: {
+//       defaultTheme: 'customTeheme',
+//       themes: {
+//         customTheme,
+//       },
+//     },
+//   })
