aba976d46da5ea09673cf49de57c24cb49856d5b
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-04-18 23:46:30 +0200

    Updated anonym function

--- back/src/utils/anonym.mjs
+++ back/src/utils/anonym.mjs
@@ -1,15 +1,8 @@
-const generateUniqueEmail = () => {
-    const randomString = Math.random().toString(36).substring(2, 8);
-    const domain = 'anonym.com';
-    const email = `user_${randomString}@${domain}`;
-    return email;
-};
-
 export const anonymizeUserData = () => {
     const user = {
         firstname: "Anonyme",
         lastname: "Anonyme",
-        email: generateUniqueEmail(),
+        email: "anonym@anonym.com",
         password: "",
         phone: "",
         deleted_at: new Date()
