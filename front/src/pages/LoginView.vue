3df88314b5705d089efe9cdbc888bb7c7e6afd8e
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-04-24 15:59:08 +0200

    Added responsive for desktop

--- front/src/pages/LoginView.vue
+++ front/src/pages/LoginView.vue
@@ -11,56 +11,141 @@ import { useRouter } from "vue-router";
 const router = useRouter();
 
 const navigateBack = () => {
-  console.log("hi")
-  router.push("/")
-}
+  console.log("hi");
+  router.push("/");
+};
 </script>
 
 <template>
   <div class="container">
-    <Stack
-      width="fit-content"
-      cursor="pointer"
-      align="center"
-      justify="flex-start"
-      direction="row"
-      margin="0"
-      gap="0.5rem"
-    >
-      <ArrowLeft height="15px" width="15px" />
-      <span @click="navigateBack">Back to the website</span>
-    </Stack>
-    <h1>Welcome to our website !</h1>
-    <DividerWithText container-class="mt-12 mb-8 relative" content="Continue With" />
-    <Stack align="center" justify="center" direction="column" margin="1rem" gap="1.2rem">
-      <VBtn width="100%" height="55px" flat color="primary" variant="outlined" style="text-transform: none;">
-        Login with Google
-        <template v-slot:prepend>
-          <Google width="25px" height="25px" />
-        </template>
-      </VBtn>
-      <VBtn width="100%" height="55px" flat color="primary" variant="outlined" style="text-transform: none;">
-        <template v-slot:prepend>
-          <Apple width="25px" height="25px" />
-        </template>
-        Login with Apple</VBtn
-      >
-      <VBtn width="100%" height="55px" flat color="primary" variant="outlined" style="text-transform: none;">
-        <template v-slot:prepend>
-          <Facebook width="25px" height="25px" />
-        </template>
-        Login with Facebook</VBtn
-      >
-    </Stack>
-    <DividerWithText container-class="mt-8 mb-12 relative" content="Or log yourself" />
-    <LoginForm />
+    <div class="image-container">
+      <img src="../assets/login.jpg" />
+    </div>
+    <div class="superior-container">
+      <div class="login-container">
+        <Stack
+          width="fit-content"
+          cursor="pointer"
+          align="center"
+          justify="flex-start"
+          direction="row"
+          margin="0"
+          gap="0.5rem"
+        >
+          <ArrowLeft height="15px" width="15px" />
+          <span @click="navigateBack">Back to the website</span>
+        </Stack>
+        <h1>Welcome to our website !</h1>
+        <DividerWithText container-class="mt-12 mb-8 relative" content="Continue With" />
+        <Stack
+          align="center"
+          justify="center"
+          direction="column"
+          margin="1rem"
+          gap="1.2rem"
+        >
+          <VBtn
+            width="100%"
+            height="55px"
+            flat
+            color="primary"
+            variant="outlined"
+            style="text-transform: none"
+          >
+            Login with Google
+            <template v-slot:prepend>
+              <Google width="25px" height="25px" />
+            </template>
+          </VBtn>
+          <VBtn
+            width="100%"
+            height="55px"
+            flat
+            color="primary"
+            variant="outlined"
+            style="text-transform: none"
+          >
+            <template v-slot:prepend>
+              <Apple width="25px" height="25px" />
+            </template>
+            Login with Apple</VBtn
+          >
+          <VBtn
+            width="100%"
+            height="55px"
+            flat
+            color="primary"
+            variant="outlined"
+            style="text-transform: none"
+          >
+            <template v-slot:prepend>
+              <Facebook width="25px" height="25px" />
+            </template>
+            Login with Facebook</VBtn
+          >
+        </Stack>
+        <DividerWithText
+          container-class="mt-8 mb-12 relative"
+          content="Or log yourself"
+        />
+        <LoginForm />
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
 .container {
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+  @media only screen and (max-width: 810px) {
+    min-height: 100vh;
+    overflow-y: scroll;
+    align-items: center;
+  }
+}
+
+.image-container {
+  display: none;
+  flex: 0;
+  @media only screen and (min-width: 810px) {
+    display: block;
+    flex: 1;
+    position: relative;
+    img {
+      object-fit: cover;
+      object-position: center center;
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+
+.superior-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  padding: 2.4rem;
+  @media only screen and (max-width: 600px) {
+    padding: 00.2rem;
+  }
+  @media only screen and (min-width: 810px) {
+    max-width: 600px;
+  }
+}
+
+.login-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
   padding: 1rem;
   font-size: 0.9rem;
+  width: 100%;
+  @media only screen and (min-width: 810px) {
+    padding: 2.4rem;
+  }
   h1 {
     margin-top: 0.2rem;
     font-size: 1.3rem;
