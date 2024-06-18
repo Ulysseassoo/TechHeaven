8bc15b9908d3c7661f8089a4f8c16f65a22751f5
fabian222222 <fabian.zuo@supinternet.fr>
2024-06-18 03:08:10 +0200

    new: add button component | update homepage | add button type file

--- front/src/pages/HomeView.vue
+++ front/src/pages/HomeView.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
 import Header from "../components/Header.vue";
+import WelcomeSection from "../components/Home/WelcomeSection.vue";
+import NewProducts from "../components/Home/NewProducts.vue";
+import MostSells from "../components/Home/MostSells.vue";
+import FeedbackSection from "../components/Home/FeedbackSection.vue";
+import Footer from "../components/Footer.vue";
 </script>
 
 <template>
-  <Header />
+  <section class="container">
+    <Header />
+    <WelcomeSection />
+    <NewProducts />
+    <MostSells />
+    <FeedbackSection />
+    <Footer />
+  </section>
 </template>
+
+<style scope>
+.container {
+  background: #f8f9fc;
+}
+</style>
