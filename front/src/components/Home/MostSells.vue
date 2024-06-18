fd69640b1db252ed285e59eb6a079cc109729acc
fabian222222 <fabian.zuo@supinternet.fr>
2024-06-18 03:07:39 +0200

    new: most sells product section

---
+++ front/src/components/Home/MostSells.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import ProductCard from "./ProductCard.vue";
+</script>
+
+<template>
+  <section class="section-container">
+    <h1>Nos produits les plus vendus</h1>
+
+    <div class="products-container">
+      <ProductCard
+        style="flex: 1"
+        name="Clavier logitech G575"
+        img="src/assets/Home/welcome-new-product-keyboard.avif"
+        cta-text="Acheter"
+        :is-full-height="true"
+      />
+      <ProductCard
+        style="flex: 1"
+        name="Clavier logitech G575"
+        img="src/assets/Home/welcome-new-product-keyboard.avif"
+        cta-text="Acheter"
+        :is-full-height="true"
+      />
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.section-container {
+  height: 100vh;
+  padding: 25px;
+  display: flex;
+  flex-direction: column;
+}
+
+.products-container {
+  flex: auto;
+  display: flex;
+  gap: 15px;
+  margin-top: 20px;
+  padding-bottom: 0;
+}
+
+.products {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+</style>
