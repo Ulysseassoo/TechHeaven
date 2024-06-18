8ce619b92811115e522c85618f8af6c88e84f620
fabian222222 <fabian.zuo@supinternet.fr>
2024-06-18 03:07:21 +0200

    new: add new products section

---
+++ front/src/components/Home/NewProducts.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import ProductCard from "./ProductCard.vue";
+</script>
+
+<template>
+  <section class="section-container">
+    <h1>Nos nouveaux produits</h1>
+
+    <div class="products-container">
+      <div>
+        <ProductCard
+          name="Clavier logitech G575"
+          img="src/assets/Home/welcome-new-product-keyboard.avif"
+          cta-text="Acheter"
+          :is-full-height="true"
+        />
+      </div>
+      <div class="products">
+        <ProductCard
+          name="Sony"
+          img="src/assets/Home/sony.avif"
+          cta-text="Acheter"
+          :is-full-height="false"
+        />
+        <ProductCard
+          name="Sony"
+          img="src/assets/Home/sony.avif"
+          cta-text="Acheter"
+          :is-full-height="false"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.section-container {
+  height: 100vh;
+  padding: 25px;
+  padding-bottom: 0;
+  display: flex;
+  flex-direction: column;
+}
+
+.products-container {
+  flex: auto;
+  display: flex;
+  gap: 15px;
+  margin-top: 20px;
+}
+
+.products {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+</style>
---
+++ front/src/components/Home/ProductCard.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import Button from "../Button.vue";
+
+defineProps<{
+  name: string;
+  img: string;
+  ctaText: string;
+  isFullHeight?: boolean;
+}>();
+</script>
+
+<template>
+  <div class="container" :class="isFullHeight ? 'full-height' : 'half-height'">
+    <img class="product-image" :src="img" alt="image of the product" />
+    <div class="product-details">
+      <p class="product-details-name">{{ name }}</p>
+      <Button class="product-details-cta" content="Acheter">{{
+        ctaText
+      }}</Button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+  border-radius: 5px;
+  overflow: hidden;
+}
+.full-height {
+  height: 100%;
+}
+.half-height {
+  height: 50%;
+}
+.product-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.product-details {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  color: white;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
+}
+.product-details-name {
+  font-size: 35px;
+  text-align: center;
+  font-weight: 600;
+}
+.product-details-cta {
+  width: min-content;
+}
+</style>
