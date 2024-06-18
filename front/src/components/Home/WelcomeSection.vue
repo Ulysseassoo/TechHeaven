14e2111ac39919741524f18a63ecf7526252956e
fabian222222 <fabian.zuo@supinternet.fr>
2024-06-18 03:07:02 +0200

    new: add welcome section in home page

---
+++ front/src/components/Home/WelcomeSection.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import Button from "./../Button.vue";
+</script>
+
+<template>
+  <section class="container">
+    <img
+      class="welcome-background"
+      src="../../assets/Home/macbook-welcome-section.avif"
+      alt="Welcome background image - macbook pro"
+    />
+    <p class="welcome-message">Find what you need.</p>
+    <Button class="discover-button" content="Start to explore" />
+  </section>
+</template>
+
+<style scoped>
+.container {
+  height: calc(100vh - 70px);
+  position: relative;
+}
+
+.welcome-background {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.welcome-message {
+  font-size: 35px;
+  color: white;
+  position: absolute;
+  top: 70px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.discover-button {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 10px;
+}
+</style>
