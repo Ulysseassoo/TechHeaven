2dddc6522db1e9bf7d61649d06f5bcb658b5f950
jphayek <jphayek1@gmail.com>
2024-07-15 13:40:00 +0200

    Secure Routes

--- back/src/routes/deliveryRoutes.mjs
+++ back/src/routes/deliveryRoutes.mjs
@@ -1,10 +1,11 @@
 import express from 'express';
 import Delivery from "../models/Delivery.mjs";
+import {shouldBeAuthenticate, shouldBeAdmin} from "../middlewares/authentication.mjs";
 
 const router = express.Router();
 
 // Ajouter une nouvelle livraison
-router.post('/deliveries', async (req, res) => {
+router.post('/deliveries',shouldBeAuthenticate, async (req, res) => {
     const delivery = new Delivery(req.body);
     try {
         await delivery.save();
@@ -15,7 +16,7 @@ router.post('/deliveries', async (req, res) => {
 });
 
 // Récupérer toutes les livraisons
-router.get('/deliveries', async (req, res) => {
+router.get('/deliveries',shouldBeAuthenticate, async (req, res) => {
     try {
         const deliveries = await Delivery.find({});
         res.status(200).send(deliveries);
@@ -26,7 +27,7 @@ router.get('/deliveries', async (req, res) => {
 
 
 // Mettre à jour le statut de livraison
-router.patch('/deliveries/:id', async (req, res) => {
+router.patch('/deliveries/:id',shouldBeAuthenticate, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['address', 'status', 'following_number', 'delivered'];
     const isValidOperation = updates.every(update => allowedUpdates.includes(update));
