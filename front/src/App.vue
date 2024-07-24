20d88231d359e151a12a971f3382d1456af5ce93
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-07-25 00:43:13 +0200

    fix

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,20 @@
 <script setup lang="ts">
 import { useProductStore } from "@/store/productStore";
 import { useBasketStore } from "@/store/basketStore";
+import { useUserStore } from "@/store/UserStore";
+import { onMounted } from "vue";
 
 const productStore = useProductStore();
 const basketStore = useBasketStore();
+const userStore = useUserStore();
 
-productStore.fetchProducts();
-basketStore.fetchBasket();
-basketStore.fetchBasketProducts();
+onMounted(async () => {
+  if (userStore.user) {
+    productStore.fetchProducts();
+    basketStore.fetchBasket();
+    basketStore.fetchBasketProducts();
+  }
+});
 </script>
 
 <template>
