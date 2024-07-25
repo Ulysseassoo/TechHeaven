1331d35e9f22af99f3d765651c1e8d64d94e1e82
Fabian Zuo <fzuo@dial-once.com>
2024-07-25 03:25:00 +0200

    comments unused codes & forgot auth

--- back/src/routes/invoice.mjs
+++ back/src/routes/invoice.mjs
@@ -84,226 +84,226 @@ router.post('/invoices/:orderId', shouldBeAuthenticate ,async (req, res) => {
 });
 
 // Générer le PDF de la facture
-router.get('/invoices/:id/pdf', async (req, res) => {
-  try {
-      const { id } = req.params;
+// router.get('/invoices/:id/pdf', async (req, res) => {
+//   try {
+//       const { id } = req.params;
 
-      const invoice = await db.invoice.findUnique({
-          where: { id: parseInt(id) },
-          include: { user: true, product: true }
-      });
+//       const invoice = await db.invoice.findUnique({
+//           where: { id: parseInt(id) },
+//           include: { user: true, product: true }
+//       });
 
-      if (!invoice) {
-          return res.status(404).json({ status: 404, message: "Facture non trouvée" });
-      }
+//       if (!invoice) {
+//           return res.status(404).json({ status: 404, message: "Facture non trouvée" });
+//       }
 
-      // Générer le PDF
-      const doc = new PDFDocument();
-      let buffers = [];
-      doc.on('data', buffers.push.bind(buffers));
-      doc.on('end', () => {
-          let pdfData = Buffer.concat(buffers);
-          res.writeHead(200, {
-              'Content-Length': Buffer.byteLength(pdfData),
-              'Content-Type': 'application/pdf',
-              'Content-Disposition': `attachment;filename=invoice_${id}.pdf`,
-          }).end(pdfData);
-      });
+//       // Générer le PDF
+//       const doc = new PDFDocument();
+//       let buffers = [];
+//       doc.on('data', buffers.push.bind(buffers));
+//       doc.on('end', () => {
+//           let pdfData = Buffer.concat(buffers);
+//           res.writeHead(200, {
+//               'Content-Length': Buffer.byteLength(pdfData),
+//               'Content-Type': 'application/pdf',
+//               'Content-Disposition': `attachment;filename=invoice_${id}.pdf`,
+//           }).end(pdfData);
+//       });
 
-      doc.fontSize(25).text(`Facture ${id}`, { align: 'center' });
-      doc.fontSize(18).text(`Utilisateur: ${invoice.user.email}`);
-      doc.text(`Produit: ${invoice.product.name}`);
-      doc.text(`Quantité: ${invoice.quantity}`);
-      doc.text(`Montant: ${invoice.amount} EUR`);
-      doc.text(`Status: ${invoice.status}`);
-      doc.end();
-  } catch (error) {
-      return res.status(500).json({ status: 500, message: "Erreur lors de la génération du PDF", error: error.message });
-  }
-});
+//       doc.fontSize(25).text(`Facture ${id}`, { align: 'center' });
+//       doc.fontSize(18).text(`Utilisateur: ${invoice.user.email}`);
+//       doc.text(`Produit: ${invoice.product.name}`);
+//       doc.text(`Quantité: ${invoice.quantity}`);
+//       doc.text(`Montant: ${invoice.amount} EUR`);
+//       doc.text(`Status: ${invoice.status}`);
+//       doc.end();
+//   } catch (error) {
+//       return res.status(500).json({ status: 500, message: "Erreur lors de la génération du PDF", error: error.message });
+//   }
+// });
 
-// Envoyer la facture par email
-router.post('/invoices/:invoiceId/email', async (req, res) => {
-  try {
-      const invoiceId = parseInt(req.params.invoiceId, 10);
-      const invoice = await db.invoice.findUnique({
-          where: { id: invoiceId },
-          include: { user: true },
-      });
+// // Envoyer la facture par email
+// router.post('/invoices/:invoiceId/email', async (req, res) => {
+//   try {
+//       const invoiceId = parseInt(req.params.invoiceId, 10);
+//       const invoice = await db.invoice.findUnique({
+//           where: { id: invoiceId },
+//           include: { user: true },
+//       });
 
-      if (!invoice) {
-          return res.status(404).json({ status: 404, message: 'Invoice not found' });
-      }
+//       if (!invoice) {
+//           return res.status(404).json({ status: 404, message: 'Invoice not found' });
+//       }
 
-      await sendInvoiceEmail(invoice.user.email, invoice);
+//       await sendInvoiceEmail(invoice.user.email, invoice);
 
-      res.status(200).json({ status: 200, message: 'Invoice email sent successfully' });
-  } catch (error) {
-      res.status(500).json({ status: 500, message: 'Error sending invoice email', error: error.message });
-  }
-});
+//       res.status(200).json({ status: 200, message: 'Invoice email sent successfully' });
+//   } catch (error) {
+//       res.status(500).json({ status: 500, message: 'Error sending invoice email', error: error.message });
+//   }
+// });
 
-router.post("/invoices/:invoiceId/pay", async (req, res) => {
-  const { invoiceId } = req.params;
+// router.post("/invoices/:invoiceId/pay", async (req, res) => {
+//   const { invoiceId } = req.params;
 
-  try {
+//   try {
 
-    const invoiceObjectId = new mongoose.Types.ObjectId(invoiceId);
+//     const invoiceObjectId = new mongoose.Types.ObjectId(invoiceId);
 
-    const invoice = await Invoice.findById(invoiceObjectId);
+//     const invoice = await Invoice.findById(invoiceObjectId);
 
-    if (!invoice) {
-      return res.status(404).json({ message: "Facture non trouvée" });
-    }
+//     if (!invoice) {
+//       return res.status(404).json({ message: "Facture non trouvée" });
+//     }
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: invoice.amount * 100, // montant en centimes
-      currency: "eur",
-      metadata: { invoiceId: invoice.id },
-    });
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: invoice.amount * 100, // montant en centimes
+//       currency: "eur",
+//       metadata: { invoiceId: invoice.id },
+//     });
 
-    return res.status(201).json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    return res.status(500).json({ message: "Erreur lors de l'initiation du paiement", error: error.message });
-  }
-});
+//     return res.status(201).json({ clientSecret: paymentIntent.client_secret });
+//   } catch (error) {
+//     return res.status(500).json({ message: "Erreur lors de l'initiation du paiement", error: error.message });
+//   }
+// });
 
-/////// Générer l'ID de la colonne paymentIntentID pour accéder au Refund /////////
+// /////// Générer l'ID de la colonne paymentIntentID pour accéder au Refund /////////
 
-router.post('/create-payment-intent', async (req, res) => {
-  const { invoiceId } = req.body;
+// router.post('/create-payment-intent', async (req, res) => {
+//   const { invoiceId } = req.body;
 
-  try {
-    const invoice = await Invoice.findById(invoiceId);
+//   try {
+//     const invoice = await Invoice.findById(invoiceId);
 
-    if (!invoice) {
-      return res.status(404).send({ message: 'Facture non trouvée' });
-    }
+//     if (!invoice) {
+//       return res.status(404).send({ message: 'Facture non trouvée' });
+//     }
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: invoice.amount * 100,
-      currency: 'eur',
-      payment_method_types: ['card'],
-      metadata: { invoiceId: invoice.id },
-    });
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount: invoice.amount * 100,
+//       currency: 'eur',
+//       payment_method_types: ['card'],
+//       metadata: { invoiceId: invoice.id },
+//     });
 
-    invoice.paymentIntentId = paymentIntent.id;
-    await invoice.save();
+//     invoice.paymentIntentId = paymentIntent.id;
+//     await invoice.save();
 
-    res.send({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    res.status(500).send({ message: 'Erreur lors de la création du paiement', error: error.message });
-  }
-});
+//     res.send({ clientSecret: paymentIntent.client_secret });
+//   } catch (error) {
+//     res.status(500).send({ message: 'Erreur lors de la création du paiement', error: error.message });
+//   }
+// });
   
 
-/// Remboursement 
-router.post('/refund', async (req, res) => {
-  const { paymentIntentId, invoiceId } = req.body;
+// /// Remboursement 
+// router.post('/refund', async (req, res) => {
+//   const { paymentIntentId, invoiceId } = req.body;
 
-  try {
-    const invoice = await Invoice.findById(invoiceId);
+//   try {
+//     const invoice = await Invoice.findById(invoiceId);
 
-    if (!invoice) {
-      console.log('Facture non trouvée');
-      return res.status(404).send({ message: 'Facture non trouvée' });
-    }
+//     if (!invoice) {
+//       console.log('Facture non trouvée');
+//       return res.status(404).send({ message: 'Facture non trouvée' });
+//     }
 
-    if (invoice.status !== 'paid') {
-      console.log("La facture n'a pas encore été payée, impossible de rembourser.");
-      return res.status(400).send({ message: "La facture n'a pas encore été payée, impossible de rembourser." });
-    }
+//     if (invoice.status !== 'paid') {
+//       console.log("La facture n'a pas encore été payée, impossible de rembourser.");
+//       return res.status(400).send({ message: "La facture n'a pas encore été payée, impossible de rembourser." });
+//     }
 
-    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+//     const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
 
-    if (!paymentIntent) {
-      console.log('Impossible de récupérer le PaymentIntent.');
-      return res.status(400).send({ message: "Impossible de récupérer le PaymentIntent." });
-    }
+//     if (!paymentIntent) {
+//       console.log('Impossible de récupérer le PaymentIntent.');
+//       return res.status(400).send({ message: "Impossible de récupérer le PaymentIntent." });
+//     }
 
-    console.log('PaymentIntent:', paymentIntent);
+//     console.log('PaymentIntent:', paymentIntent);
 
-    if (paymentIntent.status !== 'succeeded') {
-      console.log("Le PaymentIntent n'a pas réussi.");
-      return res.status(400).send({ message: "Le PaymentIntent n'a pas réussi." });
-    }
+//     if (paymentIntent.status !== 'succeeded') {
+//       console.log("Le PaymentIntent n'a pas réussi.");
+//       return res.status(400).send({ message: "Le PaymentIntent n'a pas réussi." });
+//     }
 
-    if (!paymentIntent.charges.data.length) {
-      console.log('Le PaymentIntent ne contient pas de charges.');
-      return res.status(400).send({ message: "Le PaymentIntent ne contient pas de charges." });
-    }
+//     if (!paymentIntent.charges.data.length) {
+//       console.log('Le PaymentIntent ne contient pas de charges.');
+//       return res.status(400).send({ message: "Le PaymentIntent ne contient pas de charges." });
+//     }
 
-    const chargeId = paymentIntent.charges.data[0].id;
+//     const chargeId = paymentIntent.charges.data[0].id;
 
-    console.log('Charge ID:', chargeId);
+//     console.log('Charge ID:', chargeId);
 
-    const refund = await stripe.refunds.create({ charge: chargeId });
+//     const refund = await stripe.refunds.create({ charge: chargeId });
 
-    invoice.status = 'refunded';
-    await invoice.save();
+//     invoice.status = 'refunded';
+//     await invoice.save();
 
-    res.status(200).send({ message: 'Remboursement effectué avec succès', refund });
-  } catch (error) {
-    console.log('Erreur lors du remboursement:', error.message);
-    res.status(500).send({ message: 'Erreur lors du remboursement', error: error.message });
-  }
-});
+//     res.status(200).send({ message: 'Remboursement effectué avec succès', refund });
+//   } catch (error) {
+//     console.log('Erreur lors du remboursement:', error.message);
+//     res.status(500).send({ message: 'Erreur lors du remboursement', error: error.message });
+//   }
+// });
 
 
 
-router.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
-  const sig = req.headers['stripe-signature'];
+// router.post('/webhook', express.raw({ type: 'application/json' }), async (req, res) => {
+//   const sig = req.headers['stripe-signature'];
 
-  let event;
+//   let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-  } catch (err) {
-    console.error(`Webhook Error: ${err.message}`);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
-  }
+//   try {
+//     event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+//   } catch (err) {
+//     console.error(`Webhook Error: ${err.message}`);
+//     return res.status(400).send(`Webhook Error: ${err.message}`);
+//   }
 
-  // Gérer l'événement de paiement réussi
-  if (event.type === 'payment_intent.succeeded') {
-    const paymentIntent = event.data.object;
-    const invoiceId = paymentIntent.metadata.invoiceId;
+//   // Gérer l'événement de paiement réussi
+//   if (event.type === 'payment_intent.succeeded') {
+//     const paymentIntent = event.data.object;
+//     const invoiceId = paymentIntent.metadata.invoiceId;
 
-    try {
-      const invoice = await Invoice.findByIdAndUpdate(invoiceId, { status: 'paid' }, { new: true });
+//     try {
+//       const invoice = await Invoice.findByIdAndUpdate(invoiceId, { status: 'paid' }, { new: true });
 
-      if (!invoice) {
-        return res.status(404).send('Facture non trouvée');
-      }
+//       if (!invoice) {
+//         return res.status(404).send('Facture non trouvée');
+//       }
 
-      // Envoyer la facture par email
-      await sendInvoiceEmail(invoice.userEmail, invoice);
+//       // Envoyer la facture par email
+//       await sendInvoiceEmail(invoice.userEmail, invoice);
 
-      console.log(`Facture ${invoiceId} marquée comme payée.`);
-      return res.status(200).send({ received: true });
-    } catch (error) {
-      console.error(`Erreur lors de la mise à jour de la facture ${invoiceId}: ${error.message}`);
-      return res.status(500).send({ error: error.message });
-    }
-  }
+//       console.log(`Facture ${invoiceId} marquée comme payée.`);
+//       return res.status(200).send({ received: true });
+//     } catch (error) {
+//       console.error(`Erreur lors de la mise à jour de la facture ${invoiceId}: ${error.message}`);
+//       return res.status(500).send({ error: error.message });
+//     }
+//   }
 
-  // Gérer l'événement de paiement échoué
-  if (event.type === 'payment_intent.payment_failed') {
-    const paymentIntent = event.data.object;
-    const invoiceId = paymentIntent.metadata.invoiceId;
+//   // Gérer l'événement de paiement échoué
+//   if (event.type === 'payment_intent.payment_failed') {
+//     const paymentIntent = event.data.object;
+//     const invoiceId = paymentIntent.metadata.invoiceId;
 
-    try {
-      await Invoice.findByIdAndUpdate(invoiceId, { status: 'failed' });
+//     try {
+//       await Invoice.findByIdAndUpdate(invoiceId, { status: 'failed' });
 
-      console.log(`Facture ${invoiceId} marquée comme échouée.`);
-      return res.status(200).send({ received: true });
-    } catch (error) {
-      console.error(`Erreur lors de la mise à jour de la facture ${invoiceId}: ${error.message}`);
-      return res.status(500).send({ error: error.message });
-    }
-  }
+//       console.log(`Facture ${invoiceId} marquée comme échouée.`);
+//       return res.status(200).send({ received: true });
+//     } catch (error) {
+//       console.error(`Erreur lors de la mise à jour de la facture ${invoiceId}: ${error.message}`);
+//       return res.status(500).send({ error: error.message });
+//     }
+//   }
 
-  res.status(200).send({ received: true });
-});
+//   res.status(200).send({ received: true });
+// });
 
 
 
--- back/src/routes/promotion.mjs
+++ back/src/routes/promotion.mjs
@@ -2,194 +2,194 @@ import express from "express";
 import { userValidator } from "../validator/userValidator.mjs";
 import { validationResult } from "express-validator";
 import { db } from "../utils/db.server.mjs";
-import { shouldBeAdmin } from "../middlewares/authentication.mjs";
+import { shouldBeAdmin, shouldBeAuthenticate } from "../middlewares/authentication.mjs";
 import { randomBytes } from 'crypto';
 import Promotion from "../models/Promotion.mjs";
 import { promotionValidator } from '../validator/promotionValidator.mjs'
 
 const router = express.Router();
 
-router.get("/promotions", shouldBeAdmin, async (req, res) => {
-    try {
-        const { page = 1, limit = 10, search } = req.query;
-        const query = {};
+// router.get("/promotions", shouldBeAdmin, async (req, res) => {
+//     try {
+//         const { page = 1, limit = 10, search } = req.query;
+//         const query = {};
 
-        if (search !== undefined && search !== "") {
-            const searchQuery = new RegExp(search, 'i');
-            query.$or = [
-                { type: { $regex: searchQuery } },
-            ]
-        }
+//         if (search !== undefined && search !== "") {
+//             const searchQuery = new RegExp(search, 'i');
+//             query.$or = [
+//                 { type: { $regex: searchQuery } },
+//             ]
+//         }
 
-        const promotions = await Promotion.findToClient(query, page, limit);
+//         const promotions = await Promotion.findToClient(query, page, limit);
 
-        const count = await Promotion.countDocuments(query);
+//         const count = await Promotion.countDocuments(query);
 
-        return res.status(200).json({
-            status: 200,
-            totalPages: Math.ceil(count / limit),
-            currentPage: page,
-            totalCount: count,
-            data: promotions
-        })
+//         return res.status(200).json({
+//             status: 200,
+//             totalPages: Math.ceil(count / limit),
+//             currentPage: page,
+//             totalCount: count,
+//             data: promotions
+//         })
 
-    } catch (error) {
-        return res.status(401).send({
-            status: 401,
-            message: error.message || error,
-        });
-    }
-})
+//     } catch (error) {
+//         return res.status(401).send({
+//             status: 401,
+//             message: error.message || error,
+//         });
+//     }
+// })
 
-router.get("/promotions/:id", async (req, res) => {
-    const id = req.params.id
+// router.get("/promotions/:id", shouldBeAuthenticate, async (req, res) => {
+//     const id = req.params.id
 
-    try {
+//     try {
 
-        const promotion = await Promotion.findOne({
-            id,
-        })
+//         const promotion = await Promotion.findOne({
+//             id,
+//         })
 
-        if (!promotion) {
-            return res.status(400).json({ status: 400, message: 'Promotion inexistant.' });
-        }
+//         if (!promotion) {
+//             return res.status(400).json({ status: 400, message: 'Promotion inexistant.' });
+//         }
 
-        return res.status(200).json({
-            status: 200,
-            data: promotion
-        })
-    } catch (error) {
-        console.log("🚀 ~ router.get ~ error:", error)
-        return res.status(401).send({
-            status: 401,
-            message: error.message || error,
-        });
-    }
-})
+//         return res.status(200).json({
+//             status: 200,
+//             data: promotion
+//         })
+//     } catch (error) {
+//         console.log("🚀 ~ router.get ~ error:", error)
+//         return res.status(401).send({
+//             status: 401,
+//             message: error.message || error,
+//         });
+//     }
+// })
 
-router.post("/promotions", promotionValidator, shouldBeAdmin, async (req, res) => {
-    try {
+// router.post("/promotions", promotionValidator, shouldBeAdmin, async (req, res) => {
+//     try {
 
-        const { is_one_time, expiry_date } = req.body
-        const type = randomBytes(6).toString('hex').slice(0, 6)
+//         const { is_one_time, expiry_date } = req.body
+//         const type = randomBytes(6).toString('hex').slice(0, 6)
 
-        const promotion = await db.promotion.create({
-            data: {
-                is_one_time,
-                expiry_date,
-                type,
-                created_at: new Date(),
-            },
-            select: {
-                id: true,
-                is_one_time: true,
-                expiry_date: true,
-                type: true,
-                created_at: true
-            }
-        })
+//         const promotion = await db.promotion.create({
+//             data: {
+//                 is_one_time,
+//                 expiry_date,
+//                 type,
+//                 created_at: new Date(),
+//             },
+//             select: {
+//                 id: true,
+//                 is_one_time: true,
+//                 expiry_date: true,
+//                 type: true,
+//                 created_at: true
+//             }
+//         })
 
-        return res.status(201).json({
-            status: 201,
-            data: promotion
-        })
-    } catch (error) {
-        return res.status(401).send({
-            status: 401,
-            message: error.message || error,
-        });
-    }
-})
+//         return res.status(201).json({
+//             status: 201,
+//             data: promotion
+//         })
+//     } catch (error) {
+//         return res.status(401).send({
+//             status: 401,
+//             message: error.message || error,
+//         });
+//     }
+// })
 
-router.put("/promotions/:id", promotionValidator, shouldBeAdmin, async (req, res) => {
-    const id = req.params.id
+// router.put("/promotions/:id", promotionValidator, shouldBeAdmin, async (req, res) => {
+//     const id = req.params.id
 
-    const errors = validationResult(req);
+//     const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(401).send({
-            status: 401,
-            message: errors.formatWith(({ msg, path }) => {
-                return {
-                    msg,
-                    path
-                }
-            }).array()
-        });
-    }
+//     if (!errors.isEmpty()) {
+//         return res.status(401).send({
+//             status: 401,
+//             message: errors.formatWith(({ msg, path }) => {
+//                 return {
+//                     msg,
+//                     path
+//                 }
+//             }).array()
+//         });
+//     }
 
-    const { isOneTime, expiryDate } = req.body;
+//     const { isOneTime, expiryDate } = req.body;
 
-    try {
-        const promotion = await db.promotion.findUnique({
-            where: {
-                id
-            }
-        })
+//     try {
+//         const promotion = await db.promotion.findUnique({
+//             where: {
+//                 id
+//             }
+//         })
 
-        if (!promotion) {
-            return res.status(400).json({ status: 400, message: 'Promotion inexistant.' });
-        }
+//         if (!promotion) {
+//             return res.status(400).json({ status: 400, message: 'Promotion inexistant.' });
+//         }
 
-        const updatedPromotion = await db.promotion.update({
-            where: {
-                id
-            },
-            data: {
-                isOneTime,
-                expiryDate,
-            },
-            select: {
-                isOneTime: true,
-                expiryDate: true,
-                created_at: true,
-                id: true,
-                type: true,
-            }
-        })
+//         const updatedPromotion = await db.promotion.update({
+//             where: {
+//                 id
+//             },
+//             data: {
+//                 isOneTime,
+//                 expiryDate,
+//             },
+//             select: {
+//                 isOneTime: true,
+//                 expiryDate: true,
+//                 created_at: true,
+//                 id: true,
+//                 type: true,
+//             }
+//         })
 
-        return res.status(200).json({
-            status: 200,
-            data: updatedPromotion
-        })
-    } catch (error) {
-        return res.status(401).send({
-            status: 401,
-            message: error.message || error,
-        });
-    }
-})
+//         return res.status(200).json({
+//             status: 200,
+//             data: updatedPromotion
+//         })
+//     } catch (error) {
+//         return res.status(401).send({
+//             status: 401,
+//             message: error.message || error,
+//         });
+//     }
+// })
 
-router.delete("/promotions/:id", shouldBeAdmin, async (req, res) => {
-    const id = req.params.id
+// router.delete("/promotions/:id", shouldBeAdmin, async (req, res) => {
+//     const id = req.params.id
 
-    try {
-        const promotion = await db.promotion.findUnique({
-            where: {
-                id,
-            }
-        })
+//     try {
+//         const promotion = await db.promotion.findUnique({
+//             where: {
+//                 id,
+//             }
+//         })
 
-        if (!promotion) {
-            return res.status(400).json({ status: 400, message: 'Promotion inexistant.' });
-        }
+//         if (!promotion) {
+//             return res.status(400).json({ status: 400, message: 'Promotion inexistant.' });
+//         }
 
-        await db.promotion.delete({
-            where: {
-                id
-            }
-        })
+//         await db.promotion.delete({
+//             where: {
+//                 id
+//             }
+//         })
 
-        return res.status(200).json({
-            status: 200,
-            message: "Promotion supprimé avec succès"
-        })
-    } catch (error) {
-        return res.status(401).send({
-            status: 401,
-            message: error.message,
-        });
-    }
-})
+//         return res.status(200).json({
+//             status: 200,
+//             message: "Promotion supprimé avec succès"
+//         })
+//     } catch (error) {
+//         return res.status(401).send({
+//             status: 401,
+//             message: error.message,
+//         });
+//     }
+// })
 
 export default router;
